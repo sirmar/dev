290d20d568ee1ca474271e17d5387209b24a271a
Marcus Veibäck <sirmar@gmail.com>
2026-04-12 23:48:04 +0200

    Scope envsubst to defined vars in nginx entrypoint

--- images/nginx/scripts/serve-entrypoint.sh
+++ images/nginx/scripts/serve-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -f /etc/nginx/conf.d/default.conf.template ]; then
-	envsubst </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+	envsubst "$(env | cut -d= -f1 | sed 's/^/$/g' | tr '\n' ' ')" </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
 fi
 exec nginx -g "daemon off;"
